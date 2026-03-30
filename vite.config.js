55d9c0b8e477a908f218b26cfa360c6e57c73326
JK <josue.kpodo@gmail.com>
2026-03-30 16:58:45 -0400

    rm CI config in favor or IIS deployment & updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-VITE_BASE_PATH=/awm-bench/
-
 export default defineConfig({
   plugins: [vue()],
   base: process.env.VITE_BASE_PATH || '/',
